ca8326a1833b96b7515de80ecb38248297683a3e
“luca-pernigo” <luca.pernigo@usi.ch>
2024-02-17 16:19:44 +0100

    phi inverse computed by newtons method

--- src/CDF.py
+++ src/CDF.py
@@ -1,4 +1,6 @@
 import numpy as np
+from src.miscellaneous import find
+
 def cdf_funct(x):
     ans=np.arange(len(x)) / float(len(x)-1)
     return ans
@@ -18,3 +20,56 @@ def inv_cdf_funct(y,x):
     # without the additional x
     ans=  sorted_x_axis[int(idx_pos)-1]
     return ans
+
+
+
+
+def Phi(xn, xcdf, ycdf):
+    i=find(xn, xcdf)
+    if(xn<xcdf.min()):
+        ans=0
+        return ans
+    if(xn>xcdf.max()):
+        ans=1
+        return ans
+    
+
+    ans=ycdf[i]*(xcdf[i+1]-xn)/(xcdf[i+1]-xcdf[i])+ycdf[i+1]*(xn-xcdf[i])/(xcdf[i+1]-xcdf[i])
+    return ans
+
+
+
+
+def phi(xn, xpdf, ypdf):
+    i=find(xn, xpdf)
+    if(xn<xpdf.min()):
+        ans=ypdf[0]
+        return ans
+    if(xn>xpdf.max()):
+        ans=ypdf[-1]
+        return ans
+    
+
+    ans=ypdf[i]*(xpdf[i+1]-xn)/(xpdf[i+1]-xpdf[i])+ypdf[i+1]*(xn-xpdf[i])/(xpdf[i+1]-xpdf[i])
+    
+    return ans
+
+
+
+
+def phi_inv(u, xpdf,ypdf, xcdf, ycdf, tol=1e-3):
+    xn=0
+    bol=True
+    
+    if(u==1):
+        return xpdf[-1]
+    
+    while(bol):
+        xnew=xn-(Phi(xn,xcdf, ycdf)-u)/phi(xn, xpdf, ypdf)
+        if(abs(xn-xnew)<tol):
+            bol=False
+    
+        xn=xnew
+        # print(xn)
+    
+    return xn
