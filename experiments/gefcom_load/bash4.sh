ef3249c57190b54b6e971c3fcca8dcba5eac969c
“luca-pernigo” <luca.pernigo@usi.ch>
2024-05-21 13:30:24 +0200

    dirclean

--- experiments/gefcom_load/bash4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# code to extract the timestamp and zoneid from test data so that the prediction is in the same format of guidelines
-
-
-for((i=1;i<=15;++i))
-do
-    cut -d , -f 1-2 /Users/luca/Desktop/GEFCom2014\ Data/Load/Task\ $i/L$i-test.csv >> /Users/luca/Desktop/GEFCom2014\ Data/Load/Task\ $i/L$i-timestamp.csv
-done
