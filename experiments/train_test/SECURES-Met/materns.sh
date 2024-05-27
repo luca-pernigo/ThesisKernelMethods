47de72d2c8e5c6cfbbee35ff78de304ef78ec593
“luca-pernigo” <luca.pernigo@usi.ch>
2024-05-27 16:15:56 +0200

    script to rerun materns

---
+++ experiments/train_test/SECURES-Met/materns.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# script for rerunning materns
+python experiments/train_test/SECURES-Met/secures_qr.py "DE" "matern_1.5"
+python experiments/train_test/SECURES-Met/secures_qr.py "DE" "matern_2.5"
+
+
+python experiments/train_test/SECURES-Met/secures_qr.py "CH" "a_laplacian"
+python experiments/train_test/SECURES-Met/secures_qr.py "CH" "gaussian_rbf"
+python experiments/train_test/SECURES-Met/secures_qr.py "CH" "matern_0.5"
+python experiments/train_test/SECURES-Met/secures_qr.py "CH" "matern_1.5"
+python experiments/train_test/SECURES-Met/secures_qr.py "CH" "matern_2.5"
+
+
+python experiments/train_test/SECURES-Met/secures_qr.py "AT" "a_laplacian"
+python experiments/train_test/SECURES-Met/secures_qr.py "AT" "gaussian_rbf"
+python experiments/train_test/SECURES-Met/secures_qr.py "AT" "matern_0.5"
+python experiments/train_test/SECURES-Met/secures_qr.py "AT" "matern_1.5"
+python experiments/train_test/SECURES-Met/secures_qr.py "AT" "matern_2.5"
